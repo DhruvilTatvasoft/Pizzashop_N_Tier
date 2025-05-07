f6d7790701ce6a886e511eb92fc43c677dfdb187
Dhruvil_chotaliya <chotaliyadhruvil2309@gmail.com>
2025-05-07 19:14:46 +0530

    role based views are separeted

--- BAL/obj/Debug/net7.0/BAL.AssemblyInfo.cs
+++ BAL/obj/Debug/net7.0/BAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0287d684f0920d54028177ff4fa96df328aaf7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1426338ad3713406d87c1fe2cdfc64e8b9084d32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
